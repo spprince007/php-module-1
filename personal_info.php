950c233fb8f2e4895f319cb5a8e0339da765bfb2
unknown <spprince007>
2023-09-14 23:22:55 +0600

    personal info

---
+++ personal_info.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Md Shazadur Rahman Prince";
+$age = 28;
+$country = "Bangladesh";
+$intro = "As a software engineer is my goal, thats whay i am start the course to setup my future, I hope I will be success inshallah";
+
+echo "My name is $name"."\n"." I am $age years old"."\n"."I live in $country"."\n"."My goal: $intro";
